920050d4aa3891cf57c9b992ade1d83c82420c63
gabriel.joseph4 <gabriel.joseph4@gmail.com>
2020-04-28 19:32:49 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,4 @@
+binding.pry
 class Waiter
  
   attr_accessor :name, :yrs_experience
@@ -27,6 +28,7 @@ class Waiter
   def best_tipper
     best_tipped_meal = meals.max do |meal_a, meal_b|
       meal_a.tip <=> meal_b.tip
+      binding.pry
     end
     best_tipped_meal.customer
   end
